ec4b4540d54a9a403e40e48f5c54d9e79977f6f8
Gu Weigang <guweigang@outlook.com>
2014-04-13 18:07:00 +0800

    add createGuid

--- apps/library/BullSoft/Utility.php
+++ apps/library/BullSoft/Utility.php
@@ -206,6 +206,31 @@ class Utility
         }
         return intval(exec("wc -l " . $path));
     }
+
+    public static function createGuid($namespace = '')
+    {
+        $guid = '';
+        $uid = uniqid("", true);
+        $data = $namespace;
+        $data .= $_SERVER['REQUEST_TIME'];
+        $data .= $_SERVER['HTTP_USER_AGENT'];
+        $data .= $_SERVER['SERVER_ADDR'];
+        $data .= $_SERVER['SERVER_PORT'];
+        $data .= $_SERVER['REMOTE_ADDR'];
+        $data .= $_SERVER['REMOTE_PORT'];
+        $hash = strtoupper(hash('ripemd128', $uid . $guid . md5($data)));
+        $guid =
+            substr($hash,  0,  8) . 
+            '-' .
+            substr($hash,  8,  4) .
+            '-' .
+            substr($hash, 12,  4) .
+            '-' .
+            substr($hash, 16,  4) .
+            '-' .
+            substr($hash, 20, 12);
+        return $guid;
+    }    
 }
 
 /* Utility.php ends here */
